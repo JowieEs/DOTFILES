991a1d8123cf5d1a36ec8bad6f496f9c29982f3f
Jowiies <joel.esteller@estudiantat.upc.edu>
2026-08-15 18:01:14 +0200

    powermenu updated

--- sway/scripts/powermenu.sh
+++ sway/scripts/powermenu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the options
-options="  Lock\n󰤄  Suspend\n󰍃  Logout\n󰜉  Reboot\n󰒲  Hibernate\n󰐥  Shutdown"
+options="󰤄  Suspend\n󰍃  Logout\n󰜉  Reboot\n󰒲  Hibernate\n󰐥  Shutdown"
 
 # Pipe them into fzf
 choice=$(echo -e "$options" | fzf \
@@ -12,22 +12,6 @@ choice=$(echo -e "$options" | fzf \
 
 # Execute the chosen command
 case "$choice" in
-    *"Lock") swaylock \
-      --screenshots \
-      --clock \
-      --text-color ffffff \
-      --indicator \
-      --indicator-radius 100 \
-      --indicator-thickness 7 \
-      --effect-blur 7x5 \
-      --effect-vignette 0.5:0.5 \
-      --ring-color e60012 \
-      --key-hl-color e60012 \
-      --line-color 00000000 \
-      --inside-color 00000088 \
-      --separator-color 00000000 \
-      --fade-in 0.2 \
-      --daemonize;;
     *"Suspend") systemctl suspend ;;
     *"Logout") swaymsg exit ;;
     *"Reboot") systemctl reboot ;;
